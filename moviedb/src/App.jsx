4bd749e6376bcbcd2e8a89818bec2067d15ee19d
Jonas Stenelind <jstenelind@gmail.com>
2024-04-18 11:01:37 +0200

    Api fix

--- moviedb/src/App.jsx
+++ moviedb/src/App.jsx
@@ -1,18 +1,28 @@
 import './index.css'
 import './App.css'
-import Header from './components/header/Header'
+import axios from 'axios'
+import useStore from './store';
 
+import Header from './components/header/Header'
 import HomePage from './pages/homepage/HomePage'
 import SingleMoviePage from './pages/singlemoviepage/SingleMoviePage'
 
-
 function App() {
+  const setMovie = useStore(state => state.setMovie);
+
+  const fetchMovie = (movieId) => {
+    axios.get(`http://www.omdbapi.com/?apikey=be1d2151&i=${movieId}`)
+      .then(response => {
+        setMovie(response.data); // Uppdaterar store.js med filmer
+      })
+      .catch(error => console.log('Error:', error));
+  };
 
   return (
     <div className="app">    
       <Header/>
-      <HomePage />
-      {/* <SingleMoviePage /> */}
+      <HomePage onMovieClick={fetchMovie} />
+      <SingleMoviePage />
     </div>
   )
 }
