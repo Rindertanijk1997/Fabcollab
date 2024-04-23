87a1f4608767c9f2c55671cca4c33d025d998443
Adam Rehal <Niklasn89@gmail.com>
2024-04-23 11:33:36 +0200

    hejpåreigen

---
+++ moviedb/src/components/animation/Animation.jsx
@@ -0,0 +1,11 @@
+import './animation.css'
+
+function Animation() {
+  return (
+    <section className='animation-container'>
+        <p className='animation-streaming'>Stream here</p>
+    </section>
+  )
+}
+
+export default Animation
