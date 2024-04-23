ae8a6656ab3f2faff0d7fea6b9a56c965530b7d1
Jonas Stenelind <jstenelind@gmail.com>
2024-04-23 09:12:03 +0200

    bugfix

--- moviedb/src/components/singelmoviecard/SingelMovieCard.jsx
+++ moviedb/src/components/singelmoviecard/SingelMovieCard.jsx
@@ -1,12 +1,14 @@
 import './singelmoviecard.css';
+import useStore from '../../store';
 
 
 function SingelMovieCard({ movie }) {
-
+    const addToWatchlist = useStore(state => state.addToWatchlist);
+    const addToFavorites = useStore(state => state.addToFavoritelist);
 
     return (
-        
-    <div className="card">
+
+        <div className="card">
             <div className="card_left">
                 <img className='movie-img' src={movie.Poster} alt="Filmlogga" />
             </div>
@@ -22,8 +24,18 @@ function SingelMovieCard({ movie }) {
                 <div className="card_right__plot">
                     <p className='movie-description'>{movie.Plot}</p>
                 </div>
+                <div className="card_right__details">
+                <ul>
+                    <li>
+                        <img className='movie-logo' src="src/assets/banner.png" onClick={() => addToWatchlist(movie)} alt="" />
+                    </li>
+                    <li>
+                        <img className='movie-logo' src="src/assets/heart.png" onClick={() => addToFavorites(movie)} alt="" />
+                    </li> 
+                </ul>
                 </div>
             </div>
+        </div>
 
     );
 }
