6b27461b279a7bd144b0e3142eccf7c58430f5c7
Jonas Stenelind <jstenelind@gmail.com>
2024-04-23 15:26:42 +0200

    byta bilder när man läger till i watchlist samt favorites

--- moviedb/src/components/singelmoviecard/SingelMovieCard.jsx
+++ moviedb/src/components/singelmoviecard/SingelMovieCard.jsx
@@ -1,12 +1,28 @@
 import './singelmoviecard.css';
+import React, { useState } from 'react';
 import useStore from '../../store';
 import banner from '../../assets/banner.png';
+import bannerFull from '../../assets/banner-full.png'
+import brokenHeart from '../../assets/broken-heart.png';
 import heart from '../../assets/heart.png';
 
 
 function SingelMovieCard({ movie }) {
     const addToWatchlist = useStore(state => state.addToWatchlist);
     const addToFavorites = useStore(state => state.addToFavoritelist);
+    
+    const [watchlisted, setWatchlist] = useState(false); 
+    const [favorite, setFavorite] = useState(false); 
+
+    const toggleWatchlist = () => {
+        addToWatchlist(movie);
+        setWatchlist(!watchlisted); 
+    };
+
+    const toggleFavorite = () => {
+        addToFavorites(movie);
+        setFavorite(!favorite); 
+    };
 
     return (
 
@@ -27,14 +43,24 @@ function SingelMovieCard({ movie }) {
                     <p className='movie-description'>{movie.Plot}</p>
                 </div>
                 <div className="card_right__logo">
-                <ul className='ul-logo'>
-                    <li className='li-logo'>
-                    <img className='movie-logo' src={banner} onClick={() => addToWatchlist(movie)} alt="Banner" />
-                    </li>
-                    <li className='li-logo'>
-                        <img className='movie-logo' src={heart} onClick={() => addToFavorites(movie)} alt="Heart" />
-                    </li> 
-                </ul>
+                    <ul className='ul-logo'>
+                        <li className='li-logo'>
+                        <img
+                                className='movie-logo'
+                                src={watchlisted ? bannerFull : banner} 
+                                onClick={toggleWatchlist}
+                                alt={watchlisted ? "Banner" : "Banner-full"}
+                            />
+                        </li>
+                        <li className='li-logo'>
+                            <img
+                                className='movie-logo'
+                                src={favorite ? heart : brokenHeart} 
+                                onClick={toggleFavorite}
+                                alt={favorite ? "Heart" : "Broken-heart"}
+                            />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
