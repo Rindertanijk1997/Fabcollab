af6dcca7622fcb3f9027724df3a31d97cf5c1920
Jonas Stenelind <jstenelind@gmail.com>
2024-04-24 09:39:32 +0200

    bugfix

--- moviedb/src/pages/favoritespage/FavoritesPage.jsx
+++ moviedb/src/pages/favoritespage/FavoritesPage.jsx
@@ -1,16 +1,10 @@
 import './favoritespage.css'
 import useStore from '../../store';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react'
 
 function FavoritesPage() {
     const favoritelist = useStore(state => state.favoritelist); // Hämta watchlist från store.js
 
-    useEffect (() => {
-        console.log(favoritelist)
-    }, [])
-
-    
     return (
         <section className='favorite-wrapper'>
             <article className='favorite-title'>
@@ -18,13 +12,15 @@ function FavoritesPage() {
             </article>
             <article className='favorite-container'>
                 {favoritelist.map(movie => (
-                    <Link className="movie-link" to={`/single-movie-page/${movie.imdbID}`}>
-                    <article key={movie.imdbID} className='favorite-box'>
-                        <img src={movie.Poster} alt={movie.Title} className='favorite-box__poster' />
-                        <h2 className='favorite-box__title'>{movie.Title}</h2>
-                        <h3 className='favorite-box__year'>{movie.Year}</h3>
-                    </article>
-                    </Link>
+                    <div key={movie.imdbID}>
+                        <Link className="movie-link" to={`/single-movie-page/${movie.imdbID}`}>
+                            <article key={movie.imdbID} className='favorite-box'>
+                                <img src={movie.Poster} alt={movie.Title} className='favorite-box__poster' />
+                                <h2 className='favorite-box__title'>{movie.Title}</h2>
+                                <h3 className='favorite-box__year'>{movie.Year}</h3>
+                            </article>
+                        </Link>
+                    </div>
                 ))}
             </article>
         </section>
--- moviedb/src/pages/watchlistpage/WatchlistPage.jsx
+++ moviedb/src/pages/watchlistpage/WatchlistPage.jsx
@@ -7,16 +7,18 @@ function WatchlistPage() {
 
     return (
         <section className='watchlist-wrapper'>
-                <h1 className='watchlist-title'>Movies to watch</h1>
+            <h1 className='watchlist-title'>Movies to watch</h1>
             <article className='watchlist-container'>
                 {watchlist.map(movie => (
-                   <Link className="movie-link" to={`/single-movie-page/${movie.imdbID}`}>
-                    <article key={movie.imdbID} className='watchlist-box'>
-                        <img src={movie.Poster} alt={movie.Title} className='watchlist-box__poster' />
-                        <h2 className='watchlist-box__title'>{movie.Title}</h2>
-                        <h3 className='watchlist-box__year'>{movie.Year}</h3>
-                    </article>
-                    </Link>
+                    <div key={movie.imdbID}>
+                        <Link className="movie-link" to={`/single-movie-page/${movie.imdbID}`}>
+                            <article className='watchlist-box'>
+                                <img src={movie.Poster} alt={movie.Title} className='watchlist-box__poster' />
+                                <h2 className='watchlist-box__title'>{movie.Title}</h2>
+                                <h3 className='watchlist-box__year'>{movie.Year}</h3>
+                            </article>
+                        </Link>
+                    </div>
                 ))}
             </article>
         </section>
